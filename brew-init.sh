e78f88b9f7d21b938eecdfd2ec62f35ac609f0a1
Jie Chen <jiechenmc@gmail.com>
2022-10-30 13:47:09 -0400

    Update brew-init.sh

--- brew-init.sh
+++ brew-init.sh
@@ -1,6 +1,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/chenj7728/.zprofile
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/chenj7728/.zprofile
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/chenj7728/.zshrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/chenj7728/.zshrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 sudo apt-get install build-essential
 brew install gcc
