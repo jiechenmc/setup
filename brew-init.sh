7498a54c56caad84ddde0872622367bb1e2f7a94
Jie Chen <jiechenmc@gmail.com>
2022-11-01 23:27:50 -0400

    updates

--- brew-init.sh
+++ brew-init.sh
@@ -1,6 +1,7 @@
+sudo apt update && sudo apt upgrade -y
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/chenj7728/.zshrc
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/chenj7728/.zshrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-sudo apt-get install build-essential
+sudo apt-get install build-essential -y
 brew install gcc
--- zsh-init.sh
+++ zsh-init.sh
@@ -1,5 +1,4 @@
 #ohmyzsh
-sudo apt update && sudo apt upgrade -y
 sudo apt install zsh
 sudo apt install curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
