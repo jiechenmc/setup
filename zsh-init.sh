bb11ef05b57b04e5dbeb8cf0e83e31c1d5ff0b5a
Jie Chen <jiechenmc@gmail.com>
2022-11-01 23:27:48 -0400

    install ubuntu updates

--- zsh-init.sh
+++ zsh-init.sh
@@ -1,4 +1,5 @@
 #ohmyzsh
+sudo apt update && sudo apt upgrade -y
 sudo apt install zsh
 sudo apt install curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
